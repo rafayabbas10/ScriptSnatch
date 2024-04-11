7fca1ec494e239bc02bc2cb6f4e1f93675f5cf18
Rafay Abbas <rafayabbas10@gmail.com>
2024-04-11 14:23:06 +0500

    limlo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -39,7 +39,7 @@ export default async function DashboardPage() {
                 </div>
 
                 <Button asChild>
-                    <Link href="/dashboard/new">+ new</Link>
+                    <Link href="/dashboard/new">Create New Article</Link>
                 </Button>
             </div>
 
